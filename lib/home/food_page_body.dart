1bfd710a71e9298e4528276979b9900b11f9157b
Mukesh Soni <imukeshsonii@gmail.com>
2022-04-15 22:31:52 +0530

    Added animation

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -13,6 +13,24 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController _pageController = PageController(viewportFraction: 0.85);
+  var _currentPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  double _height = 220;
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+        _currentPageValue = _pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -28,75 +46,103 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Stack(children: [
-      Container(
-        height: 220,
-        margin: const EdgeInsets.only(left: 5, right: 5),
-        decoration: BoxDecoration(
-          color: const Color(0xFF69c5df),
-          borderRadius: BorderRadius.circular(30),
-          image: const DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage("assets/image/food0.png"),
-          ),
-        ),
-      ),
-      Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          height: 120,
-          margin: const EdgeInsets.only(left: 30, right: 30, bottom: 15),
+    Matrix4 matrix4 = new Matrix4.identity();
+    if (index == _currentPageValue.floor()) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix4 = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix4 = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix4 = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() - 1) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix4 = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix4 = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix4 = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 0);
+    }
+
+    return Transform(
+      transform: matrix4,
+      child: Stack(children: [
+        Container(
+          height: 220,
+          margin: const EdgeInsets.only(left: 5, right: 5),
           decoration: BoxDecoration(
-            color: Colors.white,
+            color: const Color(0xFF69c5df),
             borderRadius: BorderRadius.circular(30),
-          ),
-          child: Container(
-            padding: const EdgeInsets.only(top: 10, left: 15, right: 15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                BigText(text: "Chinese Slides"),
-                const SizedBox(height: 15),
-                Row(
-                  children: [
-                    Wrap(
-                      children: List.generate(
-                        5,
-                        (index) => Icon(
-                          Icons.star,
-                          color: AppColors.mainColor,
-                          size: 15,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    SmallText(text: '4.5'),
-                    const SizedBox(width: 10),
-                    SmallText(text: '1287 Comments'),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                Row(
-                  children: [
-                    IconAndText(
-                        icon: Icons.circle_sharp,
-                        iconColor: AppColors.iconColor1,
-                        text: 'Normal'),
-                    IconAndText(
-                        icon: Icons.location_on,
-                        iconColor: AppColors.mainColor,
-                        text: '1.2km'),
-                    IconAndText(
-                        icon: Icons.access_time_rounded,
-                        iconColor: AppColors.iconColor2,
-                        text: '32min')
-                  ],
-                )
-              ],
+            image: const DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage("assets/image/food0.png"),
             ),
           ),
         ),
-      ),
-    ]);
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 15),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Container(
+              padding: const EdgeInsets.only(top: 10, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Chinese Slides"),
+                  const SizedBox(height: 15),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) => Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      SmallText(text: '4.5'),
+                      const SizedBox(width: 10),
+                      SmallText(text: '1287 Comments'),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    children: [
+                      IconAndText(
+                          icon: Icons.circle_sharp,
+                          iconColor: AppColors.iconColor1,
+                          text: 'Normal'),
+                      IconAndText(
+                          icon: Icons.location_on,
+                          iconColor: AppColors.mainColor,
+                          text: '1.2km'),
+                      IconAndText(
+                          icon: Icons.access_time_rounded,
+                          iconColor: AppColors.iconColor2,
+                          text: '32min')
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
   }
 }
